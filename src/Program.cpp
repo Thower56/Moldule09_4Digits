3f8a5b261cd524cfb29a9eee6d4a9c9b3281d20c
Charles Grondin <112575794+Thower56@users.noreply.github.com>
2023-12-17 17:19:35 -0500

    update

--- src/Program.cpp
+++ src/Program.cpp
@@ -4,9 +4,13 @@
 
 
 Affichage4Digits* affichage4Digits;
+int number;
 Program::Program(){
     affichage4Digits = new Affichage4Digits(new Affichage4DigitsProxyTM1637());
+    number = 0;
 }
 
 void Program::loop(){
+    affichage4Digits->afficherBinaire(number);
+    number++;
 }
